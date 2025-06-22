b79cf6804ae87cda5711fc43e1b4da283fe20249
yyj9529 <garethgates7@naver.com>
2025-06-23 04:09:48 +0900

    initial setting complete

---
+++ app/codebridge.tsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button";
+
+export default function CodeBridge() {
+    return <Button>CodeBridge</Button>
+}
+
+export const links =()=>[
+    {
+        rel:"stylesheet",
+        href:"potato.com"
+    }
+]
+
+export const meta=()=>[{title:"Tomato" , name:"Tomato" , content:"Tomato"}];
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,5 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { Button } from "~/components/ui/button";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,5 +9,5 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (<h1>home<Button>Click me</Button></h1>);
 }
